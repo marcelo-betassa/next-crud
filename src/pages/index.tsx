8422aa4c5c6c5ffef3b9469b080947e79d481ce0
marcelo-betassa <mhbeta@gmail.com>
2022-03-08 23:52:59 -0300

    config tail-wind typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <span className='text-6xl'>Next-crud</span>
+    </div>
+  )
+}
